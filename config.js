e2ef7ae443a671516a6cbd651bc8ada1ff4548ec
Vineet Velagandula <velagand@umich.edu>
2017-07-13 20:49:49 -0400

    asdf

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config = {};
 config.db = {};
 // the URL shortening host - shortened URLs will be this + base58 ID
 // i.e.: http://localhost:8456/3Ys
-config.webhost = 'https://thawing-ravine-41874.herokuapp.com/';
+config.webhost = 'localhost:' + str(process.env.PORT);
 
 // your MongoDB host and database name
 config.db.host = 'localhost';

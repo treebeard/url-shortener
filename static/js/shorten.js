e1a920be7f722e3e4228043c27c7961641490288
Vineet Velagandula <velagand@umich.edu>
2017-07-14 17:33:42 -0400

    regex fix try infinity

--- static/js/shorten.js
+++ static/js/shorten.js
@@ -9,7 +9,7 @@ $('.btn-shorten').on('click', function(){
 
   //check if url starts with http or https, if not, append http
   var protocolpattern = /^((http|https):\/\/)/;
-  var pattern = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+  var pattern = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z‌​]{2,6}\b([-a-zA-Z0-9‌​@:%_\+.~#?&=]*)/gm;
   var re_weburl = new RegExp(
     "^" +
         // protocol identifier (optional) + //
@@ -32,7 +32,7 @@ $('.btn-shorten').on('click', function(){
       $('#link').hide().fadeIn('slow');
   }
 
-  else if(!re_weburl.test(inputurl)) {
+  else if(!pattern.test(inputurl)) {
       var resultHTML = '<span>Invalid URL, are you sure you entered it correctly?</span>';
       $('#link').html(resultHTML);
       $('#link').hide().fadeIn('slow');

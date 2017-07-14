beea020c8d84b9394b195269faacfb3e88ed9d92
Vineet Velagandula <velagand@umich.edu>
2017-07-14 15:59:02 -0400

    valid url fix (hopefully)

--- static/js/shorten.js
+++ static/js/shorten.js
@@ -1,3 +1,5 @@
+var validUrl = require('valid-url');
+
 $("#url-field").keyup(function(event){
     if(event.keyCode == 13){
         $(".btn-shorten").click();
@@ -12,13 +14,44 @@ $('.btn-shorten').on('click', function(){
   var pattern = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
   var inputurl = $.trim($('#url-field').val());
 
-  if (!inputurl){
+  /**if (!inputurl){
       var resultHTML = '<span>No URL found</span>';
       $('#link').html(resultHTML);
       $('#link').hide().fadeIn('slow');
   }
 
   else if(!pattern.test(inputurl)) {
+      var resultHTML = '<span>Invalid URL, are you sure you entered it correctly?</span>';
+      $('#link').html(resultHTML);
+      $('#link').hide().fadeIn('slow');
+  }
+
+  else{
+    if (!protocolpattern.test(inputurl)){
+        inputurl = "http://" + inputurl;
+    }
+    // AJAX call to /api/shorten with the URL that the user entered in the input box
+    $.ajax({
+      url: '/api/shorten',
+      type: 'POST',
+      dataType: 'JSON',
+      data: {url: inputurl},
+      success: function(data){
+          // display the shortened URL to the user that is returned by the server
+          var resultHTML = '<a class="result" target="_blank" href="' + data.shortUrl + '">'
+              + data.shortUrl + '</a>';
+          $('#link').html(resultHTML);
+          $('#link').hide().fadeIn('slow');
+      }
+    });
+  }**/
+  if (!inputurl){
+      var resultHTML = '<span>No URL found</span>';
+      $('#link').html(resultHTML);
+      $('#link').hide().fadeIn('slow');
+  }
+
+  else if(!validUrl.isUri(inputurl)) {
       var resultHTML = '<span>Invalid URL, are you sure you entered it correctly?</span>';
       $('#link').html(resultHTML);
       $('#link').hide().fadeIn('slow');

86f8d70bfb2f9baca2a8d005e60f5d8421aa6e5c
Vineet Velagandula <velagand@umich.edu>
2017-07-13 22:34:58 -0400

    wakemydyno

--- static/js/shorten.js
+++ static/js/shorten.js
@@ -1,12 +1,14 @@
 // add an event listener to the shorten button for when the user clicks it
 $('.btn-shorten').on('click', function(){
-  // AJAX call to /api/shorten with the URL that the user entered in the input box
+
+  //check if url starts with http or https, if not, append http
   var pattern = /^((http|https|ftp):\/\/)/;
   var inputurl = $('#url-field').val();
   if(!pattern.test(inputurl)) {
       inputurl = "http://" + inputurl;
   }
 
+  // AJAX call to /api/shorten with the URL that the user entered in the input box
   $.ajax({
     url: '/api/shorten',
     type: 'POST',
